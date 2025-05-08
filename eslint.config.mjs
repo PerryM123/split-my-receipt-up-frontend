8b55757a98dbcfea0bf9ae47d46ea904a847e525
PerryM123 <perrymiranda1@yahoo.com>
2025-05-09 08:31:30 +0900

    Added eslint rule to ensure that objects with more than 2 properties will not all be on one line

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs';
+import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
   rules: {
     'vue/multi-word-component-names': 'off',
-    'vue/html-self-closing': 'off'
+    'vue/html-self-closing': 'off',
+    'object-curly-newline': [
+      'error',
+      {
+        multiline: true,
+        minProperties: 2
+      }
+    ]
   }
-});
+})
