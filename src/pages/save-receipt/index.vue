b60b269371184c808138a5ddb986b61c67d299a8
PerryM123 <perrymiranda1@yahoo.com>
2025-05-08 10:04:48 +0900

    Added features for step 2 of save-receipt

--- src/pages/save-receipt/index.vue
+++ src/pages/save-receipt/index.vue
@@ -1,7 +1,19 @@
+<!-- 
+TODO:
+- High priority:
+  - [ ] Add validation for input text box and image
+  - [ ] Consider having a text box for receipt total amount
+
+- Low priority:
+  - [ ] Add design
+  - [ ] Fix nuxt so that it will explicitly import the functions and components instead of doing auto-imports
+  - [ ] Need to convert to atomic design soon
+-->
 <template>
   <div>
     <img v-if="isLoading" src="/loading.gif" alt="Analyzing Receipt" />
-    <template v-else>
+    <template v-else-if="currentStep === STEP_1">
+      <!-- TODO: Make a component of the 1st step -->
       <h2>レシート保存</h2>
       <p>Receipt Title</p>
       <input v-model="receiptTitle" />
@@ -38,15 +50,99 @@
         <button :disabled="!selectedFile" @click="analyzeReceipt">分析</button>
       </div>
     </template>
+    <template v-else-if="currentStep === STEP_2">
+      <h2>Scanned Items</h2>
+      <div>
+        <ul>
+          <li>Perry: 260円</li>
+          <li>Hannah: 260円</li>
+          <li>Total: {{ formatPrice(receiptTotal) }}</li>
+        </ul>
+        <div>
+          <button @click="addScannedItem">Add Item</button>
+        </div>
+        <table class="theActualPriceTable">
+          <tbody>
+            <tr class="tableRow">
+              <th>Name</th>
+              <th>Price</th>
+            </tr>
+            <template v-for="(person, personIndex) in [hannahData, perryData]">
+              <tr
+                v-for="(item, itemIndex) in person.items"
+                :key="itemIndex"
+                class="tableRow"
+                :class="`tableRow__person${personIndex}`"
+              >
+                <td>{{ item.productName }}</td>
+                <td>{{ formatPrice(item.price) }}</td>
+                <td class="buttonTableItem">
+                  <button
+                    :data-index="itemIndex"
+                    :data-user-type="person.name"
+                    class="editButton"
+                    @click="(event) => handleOpenEditModal(event, item)"
+                  >
+                    Edit
+                  </button>
+                </td>
+              </tr>
+            </template>
+          </tbody>
+        </table>
+        <div>
+          <button @click="seeFinalResults">See Final Result</button>
+        </div>
+      </div>
+    </template>
+    <template v-else-if="currentStep === STEP_3">
+      <div>this is step 3</div>
+    </template>
   </div>
 </template>
 <script setup lang="ts">
+import { USERS } from '~/constants'
+import type { ItemData, ShoppingData } from '~/interfaces/shopping'
+
+// TODO: Change to type instead???
+interface ReceiptInfo {
+  item: ItemInfo[]
+  receipt_total: number
+}
+interface ItemInfo {
+  name: string
+  price_total: number
+}
+interface ReceiptInfoResponse {
+  message: string
+  receipt_info: ReceiptInfo
+  error_info?: string
+}
+
 const DEFAULT_WHO_PAID = 'perry'
 const imageSrc = ref('')
 const selectedFile = ref<File | null>(null)
 const isLoading = ref(false)
 const receiptTitle = ref('')
 const userWhoPaid = ref(DEFAULT_WHO_PAID)
+const receiptInfo = ref<ReceiptInfo>()
+const receiptTotal = ref(0)
+const perryData = ref<ShoppingData>({
+  displayName: USERS.PERRY.DISPLAY_NAME,
+  name: USERS.PERRY.NAME,
+  items: []
+})
+const hannahData = ref<ShoppingData>({
+  displayName: USERS.HANNAH.DISPLAY_NAME,
+  name: USERS.HANNAH.NAME,
+  items: []
+})
+
+const STEP_1 = 'step1'
+const STEP_2 = 'step2'
+const STEP_3 = 'step3'
+// TODO: Change back to STEP_1 after debugging
+let currentStep = STEP_2
 
 const previewImage = (event: Event) => {
   const file = (event.target as HTMLInputElement).files?.[0]
@@ -76,12 +172,27 @@ const analyzeReceipt = async () => {
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`)
     }
-    const result = await response.json()
-    console.log('perry: Analysis result:', result)
+    const receiptInfoResponse: ReceiptInfoResponse = await response.json()
+    receiptInfo.value = receiptInfoResponse.receipt_info
+    receiptTotal.value = receiptInfoResponse.receipt_info.receipt_total
+    currentStep = STEP_2
   } catch (error) {
     console.error('Error analyzing receipt:', error)
   } finally {
     isLoading.value = false
   }
+}
+
+const addScannedItem = () => {
+  console.log('perry: addScannedItem function')
+}
+const seeFinalResults = () => {
+  console.log('perry: seeFinalResults function')
+}
+const handleOpenEditModal = (event: MouseEvent, data: ItemData) => {
+  console.log('perry: handleOpenEditModal function: ', {
+    event,
+    data
+  })
 }
 </script>
