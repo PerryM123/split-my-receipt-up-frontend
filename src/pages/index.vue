11b0f984dd4127d637e9d63126a46507ef7f5373
PerryM123 <perrymiranda1@yahoo.com>
2025-05-15 08:43:14 +0900

    Added links to top page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,27 @@
 <template>
   <div>
-    This is <span class="p-4 bg-blue-100 text-blue-800">top page</span>
+    <h1>Split Me Up Before You Go-Go</h1>
+    <div class="px-2.5">
+      <NuxtLink
+        v-for="link in linkInfo"
+        :key="link.url"
+        :to="link.url"
+        class="block bg-sky-500 hover:bg-sky-700"
+      >
+        {{ link.linkText }}
+      </NuxtLink>
+    </div>
   </div>
 </template>
 <script setup>
-   // TODO
+const linkInfo = ref([
+  {
+    linkText: '新規レシート作成',
+    url: '/save-receipt'
+  },
+  {
+    linkText: 'レシート一覧',
+    url: '/receipts'
+  }
+])
 </script>
