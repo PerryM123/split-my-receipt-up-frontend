e1e26aeb9e776160b104a01c823199de2283f76f
PerryM123 <perrymiranda1@yahoo.com>
2025-05-24 11:35:33 +0900

    Added styling to TOP page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>
-    <h1>Split Me Up Before You Go-Go</h1>
-    <div class="px-2.5">
+  <div class="p-5">
+    <h1 class="text-3xl">Split Me Up Before You Go-Go</h1>
+    <div class="mt-16">
+      <!-- TODO: There must be a way to order the tailwind classes with an extension and preitter
+       -->
+       <!-- TODO: Should we make this into an atom component? -->
       <NuxtLink
         v-for="(link, linkKey) in linkInfo"
         :key="linkKey"
         :to="link.url"
-        class="block bg-sky-500 hover:bg-sky-700"
+        class="py-5 px-5 first:mt-0 mt-5 text-center border-black border border-solid rounded block bg-gray-300 hover:opacity-30 transition-all duration-700"
       >
         {{ link.linkText }}
       </NuxtLink>
