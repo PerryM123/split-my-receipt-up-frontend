01c83cdefe922b43b7cfedb7cec3dd69e02e785a
PerryM123 <perrymiranda1@yahoo.com>
2025-05-24 11:35:20 +0900

    Added NuxtLoadingIndicator

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <!-- TODO: Use NuxtTemplate -->
+    <NuxtLoadingIndicator :height="10" :throttle="0"/>
     <NuxtPage />
   </div>
 </template>
+<script setup lang="ts">
+const { start, set } = useLoadingIndicator()
+start({ force: true })
+</script>
